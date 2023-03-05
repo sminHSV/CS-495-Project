da7467a6c57d5f46dbe4eeaaaeb3e6e6397ddeef
sminHSV <skim141@crimson.ua.edu>
2023-03-05 11:50:26 -0600

    Got rid of ReferenceError

--- pages/index.js
+++ pages/index.js
@@ -3,29 +3,6 @@ import styles from "@/styles/Home.module.css"
 import Link from 'next/link'
 import useUser from "@/lib/useUser"
 
-export async function getServerSideProps(context) {
-  try {
-    await clientPromise
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
-
-    return {
-      props: { isConnected: true }
-    }
-  } catch (e) {
-    console.error(e)
-    return {
-      props: { isConnected: false }
-    }
-  }
-}
-
 export default function Home() {
   const { user } = useUser();
 
