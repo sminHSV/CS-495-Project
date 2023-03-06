1fcdf8a009d3ed7f315cb01d0f0a2d744626f951
hanapark370 <hanapark370@gmail.com>
2023-03-06 11:53:11 -0600

    Added Logo to Tab Webpage

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>UnMute</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
