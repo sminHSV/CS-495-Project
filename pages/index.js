4d533f9306a42827be8d628d12ecce1ca9e24740
aakale1 <aakale1@crimson.ua.edu>
2023-03-26 23:55:40 -0500

    design changes to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,104 @@
-import Head from "next/head"
-import styles from "@/styles/Home.module.css"
-import Link from 'next/link'
-import useUser from "@/lib/useUser"
-
-
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import useUser from "@/lib/useUser";
 
 export default function Home() {
   const { user } = useUser();
 
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Head>
         <title>UnMute</title>
         <link rel="icon" href="unMuteIcon.ico" />
       </Head>
 
-      <main>
-      <img src="/unMutelogo/unMuteIconTransparent.png" alt="UnMute Icon"></img>
-       
-        <h1 class ="frontPageh1">
-          
-          Welcome to unMute {user ? user.name : '!'}
-      
-          </h1>
+      <main className={styles.main}>
+        <div className={styles.logoWrapper}>
+          <img
+            src="/unMutelogo/unMuteIconTransparent.png"
+            alt="UnMute Icon"
+            className={styles.logo}
+          />
+        </div>
 
-        <div className="grid">
-          {user ? <>
-              <div className="card">
+        <h1 className={styles.title}>
+          Welcome to UnMute {user ? user.name : '!'}
+        </h1>
+
+        <p className={styles.about}>
+          UnMute is a platform that provides a safe and anonymous space for students to ask questions and participate in class discussions,
+          helping to overcome the barriers that prevent active engagement and promoting a more inclusive learning environment.
+        </p>
+        <div className={styles.grid}>
+          {user ? (
+            <>
+              <div className={styles.card}>
                 <Link href="/api/logout">
-                  <h3>Logout &rarr;</h3>
-                  <p>Sign out of your account</p>
+                  
+                    <h3>Logout &rarr;</h3>
+                    <p>Sign out of your account</p>
+                  
                 </Link>
               </div>
-              <div className="card">
+              <div className={styles.card}>
                 <Link href="/join">
-                  <h3>Join &rarr;</h3>
-                  <p>Join a room</p>
+                  
+                    <h3>Join &rarr;</h3>
+                    <p>Join a room</p>
+                  
                 </Link>
               </div>
 
-              { user.guest || <div className="card">
-                <Link href="/dashboard">
-                  <h3>Dashboard &rarr;</h3>
-                  <p>Visit your dashboard</p>
-                </Link>
-              </div>}
-            </> :
-            <div className="card">
+              {!user.guest && (
+                <div className={styles.card}>
+                  <Link href="/dashboard">
+                    
+                      <h3>Dashboard &rarr;</h3>
+                      <p>Visit your dashboard</p>
+                    
+                  </Link>
+                </div>
+              )}
+            </>
+          ) : (
+            <div className={styles.card}>
               <Link href="/login">
-                <h3>Login &rarr;</h3>
-                <p>Sign in to an account</p>
+                  <h3>Login &rarr;</h3>
+                  <p>Sign in to an account</p>
+
               </Link>
             </div>
-          }
+          )}
+<div className={styles.card}>
+  <Link href="/register">
+    <div>
+      <h3>Register &rarr;</h3>
+      <p>Register here for a new user</p>
+    </div>
+  </Link>
+</div>
+
+<div className={styles.card}>
+  <Link href="/contact">
+    <div>
+      <h3>Contact Us &rarr;</h3>
+      <p>Get in touch with our team</p>
+    </div>
+  </Link>
+</div>
+
+<div className={styles.card}>
+  <Link href="/FAQ">
+    <div>
+      <h3>FAQs &rarr;</h3>
+      <p>Frequently asked questions</p>
+    </div>
+  </Link>
+</div>
+
         </div>
       </main>
-
-
     </div>
-  )
+  );
 }
