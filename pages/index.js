99e9bdf80ab957ad40ba67ea60557a3a69acafc3
aakale1 <aakale1@crimson.ua.edu>
2023-04-11 11:33:31 -0500

    removed extra cards once you log in

--- pages/index.js
+++ pages/index.js
@@ -23,78 +23,72 @@ export default function Home() {
         </div>
 
         <div className="topnav">
-        <a className="active">Speak Freely, Learn Confidently</a>
+          <a className="active">Speak Freely, Learn Confidently</a>
         </div>
         <h1 className={styles.title}>
           Welcome to UnMute {user ? user.name : '!'}
         </h1>
 
         <p className={styles.about}>
-          
+
           UnMute is a platform that provides a safe and anonymous space for students to ask questions and participate in class discussions,
           helping to overcome the barriers that prevent active engagement and promoting a more inclusive learning environment.
-        
-          </p>
+
+        </p>
+        <br></br>
+
         <div className={styles.grid}>
           {user ? (
             <>
               <div className={styles.card}>
                 <Link href="/api/logout">
-                  
-                    <h3>Logout &rarr;</h3>
-                    <p>Sign out of your account</p>
-                  
+                  <h3>Logout &rarr;</h3>
+                  <p>Sign out of your account</p>
                 </Link>
               </div>
               <div className={styles.card}>
                 <Link href="/join">
-                  
-                    <h3>Join &rarr;</h3>
-                    <p>Join a room</p>
-                  
+                  <h3>Join &rarr;</h3>
+                  <p>Join a room</p>
                 </Link>
               </div>
-
               {!user.guest && (
                 <div className={styles.card}>
                   <Link href="/dashboard">
-                    
-                      <h3>Dashboard &rarr;</h3>
-                      <p>Visit your dashboard</p>
-                    
+                    <h3>Dashboard &rarr;</h3>
+                    <p>Visit your dashboard</p>
                   </Link>
                 </div>
               )}
             </>
           ) : (
-            <div className={styles.card}>
-              <Link href="/login">
+            <>
+              <div className={styles.card}>
+                <Link href="/login">
                   <h3>Login &rarr;</h3>
                   <p>Sign in to an account</p>
-
-              </Link>
-            </div>
+                </Link>
+              </div>
+              <div className={styles.card}>
+                <Link href="/register">
+                  <h3>Register &rarr;</h3>
+                  <p>Register here for a new user</p>
+                </Link>
+              </div>
+              <div className={styles.card}>
+                <Link href="/guestLogin">
+                  <h3>Guest Login &rarr;</h3>
+                  <p>Login as Guest</p>
+                </Link>
+              </div>
+            </>
           )}
-<div className={styles.card}>
-  <Link href="/register">
-    <div>
-      <h3>Register &rarr;</h3>
-      <p>Register here for a new user</p>
-    </div>
-  </Link>
-</div>
-
-<div className={styles.card}>
-  <Link href="/guestLogin">
-    <div>
-      <h3>Guest Login &rarr;</h3>
-      <p>Login as Guest</p>
-    </div>
-  </Link>
-</div>
-
         </div>
+
       </main>
     </div>
+
+
   );
 }
+
