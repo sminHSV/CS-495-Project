1a94fed69f6a7b382bf892a11038580d4310b2bf
AndrewJMayne <ajmayne@crimson.ua.edu>
2023-04-13 10:22:57 -0500

    Add css to no email found

--- pages/resetEmailNotFound.js
+++ pages/resetEmailNotFound.js
@@ -1,19 +1,34 @@
 import { useRouter } from 'next/router';
 import {useState } from 'react';
 import Link from 'next/link'
+import styles from "@/styles/Home.module.css";
 
 export default function ResetEmailUnsuccessful(){
     const router = useRouter();
 
     return(
-        <>
-        <div>
-            <p>No user with that email exists. Please try with a different email or create an account</p>
-        </div>
+        <div style={{
+            border: '1px solid black',
+            borderRadius: '5px',
+            padding: '20px',
+            backgroundColor: '#f5f5f5',
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)'
+        }}>
+        <h1 className={styles.title}>
+        No user with that email exists
+        </h1>
+        <p>Please try with a different email or create an account</p>
         <br />
+        <div className='linkCenter'>
         <Link href="/login" className='link'>Login</Link>
-        <br/>
+        </div>
+        <div className='linkCenter'>
         <Link href="/regsiter" className='link'>Create Account</Link>
-        </>
+        </div>
+
+        </div>
     );
 }
