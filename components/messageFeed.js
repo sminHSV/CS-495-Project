ddaf27a6522e53c399a934dba8e14f6c6d5a87f4
aakale1 <aakale1@crimson.ua.edu>
2023-04-20 17:06:47 -0500

    changed css to be consistent across webpages

--- components/messageFeed.js
+++ components/messageFeed.js
@@ -3,7 +3,7 @@ import { usePusher } from '@/lib/PusherContext'
 import { fetchJSON } from '@/lib/fetch'
 import { RoomContext } from '@/lib/roomContext'
 import Message from './message'
-import { saveAs } from 'file-saver'
+//import { saveAs } from 'file-saver'
 
 /**
  * Handles organising and sorting messages.
